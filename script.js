d21e5c5daab0f0916a37b59b2d262397739e26de
TheIronNugget <g.gwynn2@gmail.com>
2021-09-26 23:56:25 -0700

    updateScript
    fix01

--- script.js
+++ script.js
@@ -13,24 +13,7 @@ const godBtn = document.querySelector(".godbtn");
 const chgBtn = document.querySelector(".txtchg");
 const chg2Btn = document.querySelector(".txtchg2");
 /* Const Section NavMenu End------------------------------------- */
-/* Var Timer------------------------------------- */
-// Set the date we're counting down to
-var countDownDate = new Date("Sep 29, 2021 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 /* Game var----------------------------------------------- */
 
@@ -131,15 +114,37 @@ function myFunction() {
 
   } 
 /* copy bar End--------------------*/ 
-/*Timer----------------------------------------- */
-// Display the result in the element with id="demo"
-document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-+ minutes + "m " + seconds + "s ";
 
-// If the count down is finished, write some text
-if (distance < 0) {
-  clearInterval(x);
-  document.getElementById("demo").innerHTML = "EXPIRED";
-}
+
+/*Canvas-----------------------------*/
+
+
+/*Canvas End-------------------------*/
+
+var countDownDate = new Date("Sep 30, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
 }, 1000);
-/*Timer----------------------------------------- */
